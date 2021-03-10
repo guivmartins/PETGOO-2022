15773b1238b6aeb6305b3a7d600b922943a1daee
guivmartins <guivmartins@gmail.com>
2021-03-09 22:24:56 -0300

    teste

---
+++ db.js
@@ -0,0 +1,11 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://guivmartins:senha1234@cluster0.w9jfr.mongodb.net/PETGOO?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+client.connect(err => {
+	const collection = client.db("test").collection("devices");
+	// perform actions on the collection object
+	client.close();
+});
